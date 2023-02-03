f0b6c706a615319463a3970336d3c0c66ab0263b
amanda <amandareisdw@gmail.com>
2023-02-03 17:55:00 -0300

    Refactor: Removed view from develop branch

--- src/main/view/index.ts
+++
@@ -1,7 +0,0 @@
-const buttonTest = document.querySelector('.button-test') as Element
-
-function sayHello (): void {
-  alert('Hello, js working!')
-}
-
-buttonTest.addEventListener('click', sayHello)
