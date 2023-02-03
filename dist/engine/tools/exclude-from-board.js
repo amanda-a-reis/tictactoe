76f48762d34bb0bca0a1738ca403c5279cdb2a44
amanda <amandareisdw@gmail.com>
2023-02-03 20:56:28 -0300

    Build - Deleted build

--- dist/engine/tools/exclude-from-board.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.excludeMovementsFromBoard = void 0;
-const crescent = (a, b) => a - b;
-const excludeMovementsFromBoard = (movements, board) => {
-    return board.filter((position) => !movements.includes(position)).sort(crescent);
-};
-exports.excludeMovementsFromBoard = excludeMovementsFromBoard;
--- dist/index.js
+++
@@ -1,2 +0,0 @@
-const o = 'o'
-const x = 'x'
