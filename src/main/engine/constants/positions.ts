e74ad505c0470c61ea91441d6b5ff6af374b7a70
amanda <amandareisdw@gmail.com>
2023-02-03 11:00:55 -0300

    Feat - Created necessary constants

---
+++ src/main/engine/constants/positions.ts
@@ -0,0 +1,12 @@
+const defaultBoard = Array.from({ length: 9 }, (value, index) => ++index)
+
+const visualDefaultBoard =
+`
+1 | 2 | 3
+---------
+4 | 5 | 6
+---------
+7 | 8 | 9
+`
+
+export { defaultBoard, visualDefaultBoard }
